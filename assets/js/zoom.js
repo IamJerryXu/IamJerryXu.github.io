80c8165e01e5fda774458a25bf49e4f2fb7b09ce
YuriXuon <150809733+IamJerryXu@users.noreply.github.com>
2026-08-21 13:24:22 +0800

    Restore TNSE aspect ratio and reduce zoom size

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,1 +1,8 @@
-$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})});
+$(document).ready(function () {
+  var isMobile = window.matchMedia("(max-width: 575.98px)").matches;
+  var zoomMargin = isMobile ? 28 : Math.min(220, Math.round(window.innerWidth * 0.15));
+  medium_zoom = mediumZoom("[data-zoomable]", {
+    background: getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color") + "ee",
+    margin: zoomMargin,
+  });
+});
